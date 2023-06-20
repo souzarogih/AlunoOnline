aa4c17db72fac550c5280f5c2c1150ff9d72c78c
HigorSouza <higor.andrade@hotmail.com>
2023-06-20 13:06:59 -0300

    Adicionando log nas classes de ContenteClasse

--- src/main/java/com/alunoonline/api/controller/ContentClassController.java
+++ src/main/java/com/alunoonline/api/controller/ContentClassController.java
@@ -2,9 +2,11 @@ package com.alunoonline.api.controller;
 
 import com.alunoonline.api.model.ContentClass;
 import com.alunoonline.api.service.ContentClassService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RestController
 @RequestMapping("/content-class")
 public class ContentClassController {
@@ -14,16 +16,19 @@ public class ContentClassController {
 
     @PostMapping
     public void saveData(@RequestBody ContentClass content_class) {
+        log.warn("ContentClassController.saveData | Cadastrando um conteúdo.");
         contentClassService.saveData(content_class);
     }
 
     @GetMapping("/{id}")
     public ContentClass findById(@PathVariable String id) {
+        log.warn("ContentClassController.findById | Localizando um conteúdo pelo id {} .", id);
         return contentClassService.findById(id);
     }
 
     @GetMapping("/list-all")
     public Iterable<ContentClass> getAllContenteClass() {
+        log.warn("ContentClassController.getAllContenteClass | Listando todo conteúdo.");
         return contentClassService.listContenteClass();
     }
 }
--- src/main/java/com/alunoonline/api/service/ContentClassService.java
+++ src/main/java/com/alunoonline/api/service/ContentClassService.java
@@ -3,11 +3,11 @@ package com.alunoonline.api.service;
 
 import com.alunoonline.api.model.ContentClass;
 import com.alunoonline.api.repository.ContentClassRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
+@Log4j2
 @Service
 public class ContentClassService {
 
@@ -15,14 +15,17 @@ public class ContentClassService {
     ContentClassRepository contentClassRepository;
 
     public void saveData(ContentClass content_class) {
+        log.warn("ContentClassService.saveData | Criando conteúdo no Redis.");
         contentClassRepository.save(content_class);
     }
 
     public ContentClass findById(String id) {
+        log.warn("ContentClassService.findById |  Obtendo um conteúdo do Redis pelo id {} .", id);
         return contentClassRepository.findById(id).orElse(null);
     }
 
     public Iterable<ContentClass> listContenteClass() {
+        log.warn("ContentClassService.listContenteClass |  Listando todo conteúdo do Redis.");
         return contentClassRepository.findAll();
     }
 
